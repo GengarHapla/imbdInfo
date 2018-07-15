670e8f365b8ec93bb946e93c5be21e1cd1d338c8
michal-krzywulicz <mkrzy@softserveinc.com>
2018-07-15 22:21:48 +0200

    .catch added for fetch action

--- src/actions/searchActions/searchActions.js
+++ src/actions/searchActions/searchActions.js
@@ -1,11 +1,12 @@
-export const SEARCH_ACTION = 'SEARCH';
-export const SEARCH_ACTION_FULFILLED = 'SEARCH_FULFILLED';
-const API_KEY = 'e260c91c';
+export const SEARCH_ACTION = "SEARCH";
+export const SEARCH_ACTION_FULFILLED = "SEARCH_FULFILLED";
+const API_KEY = "e260c91c";
 
 export function search(payload) {
   return {
     type: SEARCH_ACTION,
-    payload: fetch(`http://www.omdbapi.com/?s=${payload}&apikey=${API_KEY}`).then(response => response.json())
-  }
+    payload: fetch(`http://www.omdbapi.com/?s=${payload}&apikey=${API_KEY}`)
+      .then(response => response.json())
+      .catch(error => error)
+  };
 }
-
